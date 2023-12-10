9bff8a28da1b7b72301e86a8657cf75b62fc91c0
Hugo Pereira <renedx92@gmail.com>
2023-12-10 23:32:12 +0000

    Add hamburger menu button and toggle visibility of navigation menu

--- vite-clinic-website/main.js
+++ vite-clinic-website/main.js
@@ -1,4 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.header__nav');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('is-active');
+  hamburger.className.includes('is-active') ? navMenu.classList.add('visible') : navMenu.classList.remove('visible');
 });
