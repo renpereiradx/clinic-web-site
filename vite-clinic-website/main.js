a9e448853b0b7c1a597f4e10f53601886c748a63
Rene Pereira <renedx92@gmail.com>
2024-01-30 16:20:21 -0300

    Update header layout and styles, fix hamburger menu functionality, and add hero section buttons

--- vite-clinic-website/main.js
+++ vite-clinic-website/main.js
@@ -1,6 +1,14 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.header__nav');
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.header__nav')
+
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('is-active');
-  hamburger.className.includes('is-active') ? navMenu.classList.add('visible') : navMenu.classList.remove('visible');
-});
+  hamburger.classList.toggle('is-active')
+  hamburger.className.includes('is-active')
+    ? navMenu.classList.add('visible')
+    : navMenu.classList.remove('visible')
+})
+
+document.querySelector('#doctorsNavItemArrow').addEventListener('click', () => {
+  document.querySelector('#doctorsNavList').classList.toggle('is-active')
+  document.querySelector('#doctorsNavItemArrow').classList.toggle('is-active')
+})
